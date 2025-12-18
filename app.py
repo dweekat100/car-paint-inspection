43deb7fbe07588c13528e24be8870b4698b0b5c4
dweekat100 <168259996+dweekat100@users.noreply.github.com>
2025-12-18 13:54:54 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import streamlit.components.v1 as components
 
-with open("car_top_view.svg") as f:
+with open("car top view svg.svg") as f:
     svg = f.read()
 
 components.html(svg, height=500)
